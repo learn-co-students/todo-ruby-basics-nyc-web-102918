c2dcc12aca76e3586bf4a75f4f2b988003ccb714
GERMAIN ARTURO DURAN TORRES <arturodurantorres@gmail.com>
2018-10-10 16:26:00 +0000

    Done.

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -1,27 +1,32 @@
 def division(num1, num2)
- 
+ num3=num1/num2
+ return num3
 end
 
 def assign_variable(value)
-  
+  name= value
+  return name
 end
 
-def argue
-  
+def argue (argument)
+  return argument
 end
 
-def greeting
+def greeting (argument1, argument2)
 
 end
 
 def return_a_value
-
+ phrase="Nice"
+ return phrase
 end
 
 def last_evaluated_value
-
+ phrase="expert"
+ return phrase
 end
 
-def pizza_party
+def pizza_party (argument1='cheese')
+  return argument1
 
 end
